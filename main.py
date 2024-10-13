f889bdf699ec522094db55e254f4048ebc7ff2b6
AlejoSendra1 <alejo.sendra12@gmail.com>
2024-10-12 21:23:37 -0300

    uncomment prints

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     arr = read_input(path)
 
     sofia_sum, mateo_sum, dp = coins_game(arr)
-    #reconstruct_solution(arr, dp)
+    reconstruct_solution(arr, dp)
 
     print("\n\nGanancia Sophia:  ", sofia_sum)
     print("Ganancia Mateo:  ", mateo_sum)
